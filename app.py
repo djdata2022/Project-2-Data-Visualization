9f4c9010b89afd994633e065f6cf87fb69adcaa3
ysgorin <ysgorin@gmail.com>
2023-04-16 23:54:42 -0400

    updated flask

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 # Dependencies
+import pandas as pd
 from flask import Flask, jsonify
+
+# Read in cleaned data
+file = "ReducedCol_Public_Schools_2020-1.csv"
+data = pd.read_csv(file)
+data_json = data.to_json(orient='index')
 
 # Flask Setup
 app = Flask(__name__)
 
-@app.route("/api")
+@app.route("/")
 def data():
-    # We can import the data here once we have it all cleaned up.
-    return jsonify(data)
+    return data_json
 
 if __name__ == '__main__':
     app.run(debug=True)

ee9501b371195c2eac7a7c560520470724068840
ysgorin <ysgorin@gmail.com>
2023-04-17 19:46:49 -0400

    finished flask app

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 # Dependencies
+import numpy as np
 import pandas as pd
-from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify, render_template 
+from config import pg_user_name, pg_password
 
-# Read in cleaned data
-file = "Public_Schools_2020-2021_Cleaned.csv"
-data = pd.read_csv(file)
-data_json = data.to_json(orient='index')
+# Database Setup
+engine = create_engine(f"postgresql://{pg_user_name}:{pg_password}@localhost:5432/public_school_db")
+conn = engine.connect()
 
 # Flask Setup
 app = Flask(__name__)
 
 @app.route("/")
+def index_page():
+    return render_template("index.html")
+
+@app.route("/api")
 def data():
-    return data_json
+    df = pd.read_sql("SELECT * FROM schools_chars",conn)
+    df_json = df.to_dict(orient="records")
+    return jsonify(df_json)
 
 if __name__ == '__main__':
     app.run(debug=True)

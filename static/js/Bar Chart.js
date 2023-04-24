f7279623a97ae59121f54c70ad2279a00cd5745e
vik1023 <124318890+vik1023@users.noreply.github.com>
2023-04-24 13:57:59 -0400

    Add files via upload

--- static/js/Bar Chart.js
+++ static/js/Bar Chart.js
@@ -1,66 +1,37 @@
-var chart = new ej.charts.Chart({
-  //Initializing Primary X Axis
-  primaryXAxis: {
-      valueType: "Category",
-      title: "Cities in MD"
-  },
-  //Initializing Primary Y Axis
-  primaryYAxis: {
-      title: "Avgerage Student Teacher Ratio"
-  },
-  //Initializing Chart Series
-  series: [
-      {
-          type: "Bar",
-          dataSource: [
-              { City: "Baltimore", Average Student Teacher Ratio: 17 },
-              {City: "Cumberland", Average Student Teacher Ratio: 16 },
-              City: "Frostburg", Average Student Teacher Ratio: 16 },
-              City: "Annapolis", Average Student Teacher Ratio: 17 },
-              City: "Gambrils", Average Student Teacher Ratio: 19 },
-              City: "Edgewater", Average Student Teacher Ratio: 17 },
-              City: "Pasadena", Average Student Teacher Ratio: 16 },
-              City: "Glen Burnie", Average Student Teacher Ratio: 16 },
-              City: "FT. Meade", Average Student Teacher Ratio: 16 },
-              City: "Millersville", Average Student Teacher Ratio: 17 },
-              City: "Severna Park", Average Student Teacher Ratio: 19 },
-              City: "Hardwood", Average Student Teacher Ratio: 16 },
-              City: "Hanover", Average Student Teacher Ratio: 16 },
-              City: "Randalstown", Average Student Teacher Ratio: 15 },
-              City: "Sparow Point", Average Student Teacher Ratio: 16 },
-              City: "Towson", Average Student Teacher Ratio: 17 },
-              City: "Owings Mill", Average Student Teacher Ratio: 13 },
-              City: "Parktown", Average Student Teacher Ratio: 17 },
-              City: "Prince Federick", Average Student Teacher Ratio: 17 },
-              City: "Huntingtown", Average Student Teacher Ratio: 18 },
-              City: "Federalsburg", Average Student Teacher Ratio: 14 },
-              City: "Westminter", Average Student Teacher Ratio: 16 },
-              City: "Skyville", Average Student Teacher Ratio: 18 },
-              City: "Manchester", Average Student Teacher Ratio: 17 },
-              City: "Chesapeake City", Average Student Teacher Ratio: 13 },
-              City: "Frederick", Average Student Teacher Ratio: 18 },
-              City: "Damascus", Average Student Teacher Ratio: 15 },
-              City: "Columbia", Average Student Teacher Ratio: 17 },
-              City: "Rockville", Average Student Teacher Ratio: 16 },
-              City: "Gaithersburg", Average Student Teacher Ratio: 14 },
-              City: "Silver Spring", Average Student Teacher Ratio: 17 },
-              City: "Poolesville", Average Student Teacher Ratio: 19 },
-              City: "Germantown", Average Student Teacher Ratio: 14 },
-              City: "Bethesda", Average Student Teacher Ratio: 19 },
-              City: "Potomac", Average Student Teacher Ratio: 17 },
-              
+<!DOCTYPE html>
+<html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<body>
 
+<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
 
+<script>
+var xValues = ["Baltimore", "Cumberland", "Frostburg", "Annapolis", "Gambrils", "Edgewater", "Pasadena", "Glen Burnie", "Ft.Meade", "Millersville", "Severna Park", "Hardwood", "Hanover", "Randalstown", "Sparow Point", "Towson", "Owings Mill", "Parktown", "Prince Federick", "Huntingtown", "Federalsburg", "Westminster", "Skyville", "Manchester", "Chesapeake City", "Frederick", "Damascus", "Columbia", "Rockville", "Gaithersburg", "Silverspring", "Poolesville", "Germantown", "Bethesda","Potomac" ];
+var yValues = [17, 16, 16, 17, 19, 17, 16, 16, 16, 17, 19, 16, 16, 15, 16, 17, 13, 17, 17, 18, 14, 16, 18, 17, 13, 18, 15, 17, 16, 14, 17, 19, 14, 19, 17  ];
+var barColors = [""];
 
-          ],
-          xName: "Cities in MD",
-          yName: "Avgerage Student Teacher Ratio"
-      }
-  ]
+new Chart("myChart", {
+  type: "horizontalBar",
+  data: {
+  labels: xValues,
+  datasets: [{
+    backgroundColor: barColors,
+    data: yValues
+  }]
+},
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Maryland Public Schools"
+    },
+    scales: {
+      xAxes: [{ticks: {min: 10, max:20}}]
+    }
+  }
 });
+</script>
 
+</body>
+</html>
 
-var chart = new ej.charts.Chart({
-  title: ' Maryland Public Schools
-});
-chart.appendTo('#container');
